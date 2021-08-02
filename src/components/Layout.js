1e2ecea49ad4e4ebf36892f8da6cb982e8f84785
Lzmxya <lzmxya@gmail.com>
2021-08-02 12:54:46 +0800

    初步完成登入功能

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,9 +2,10 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 const Layout = (props) => {
+  const { auth, setAuth } = props;
   return (
     <>
-      <Header />
+      <Header auth={auth} setAuth={setAuth} />
       {props.children}
       <Footer />
     </>
