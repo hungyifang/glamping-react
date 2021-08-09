9ee58281118b5c39ee93e034f4fb748c0010479f
yeu-shiuan <83852318+yeu-shiuan@users.noreply.github.com>
2021-08-09 13:56:22 +0800

    更新營區介紹日夜動畫

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -5,8 +5,11 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-function Intro() {
+function Intro(props) {
+  const { isDay } = props;
+
   let animationContainer = createRef();
+  let animationContainerNight = createRef();
   // let animationContainermap = createRef();
 
   // const headerRef = useRef(null);
@@ -21,6 +24,15 @@ function Intro() {
     });
     anim.setSpeed(0.5);
 
+    const animnight = lottie.loadAnimation({
+      container: animationContainerNight.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      path: "/animations/intro_banner_night.json", // JSON文件路徑
+    });
+    animnight.setSpeed(0.5);
+
     LottieScrollTrigger({
       target: "#map",
       path: "/animations/intro_main.json",
@@ -31,6 +43,7 @@ function Intro() {
       markers: false,
       toggleActions: "play none none reverse",
     });
+
     function LottieScrollTrigger(vars) {
       let playhead = { frame: 0 },
         target = gsap.utils.toArray(vars.target)[0],
@@ -73,7 +86,8 @@ function Intro() {
         className="container-fluid
       p-0"
       >
-        <div ref={animationContainer}></div>
+        <div ref={animationContainer} className="intro-map"></div>
+        <div ref={animationContainerNight} className="intro-map-night"></div>
         <article id="map" className="mt-4"></article>
       </main>
     </>
