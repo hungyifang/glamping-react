68241186a8f467cbc6c64315df03c7094243a8ea
Your Name <you@example.com>
2021-08-11 15:05:05 +0800

    第一次報告後 CSS 小修正-1

--- src/pages/Event.js
+++ src/pages/Event.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/event.css";
 import $ from "jquery";
 import { withRouter } from "react-router-dom";
@@ -6,20 +6,32 @@ import EventQuickList from "../components/event/EventQuickList";
 import EventSection from "../components/event/EventSection";
 
 function Event(props) {
+  const newLoad = false;
   const isDay = props.isDay;
+  const [load, setLoad] = useState(newLoad);
+
+  function loadPage() {
+    setLoad(true);
+  }
+
+  useEffect(() => {
+    document.title = "山角行 - 當地活動";
+    loadPage();
+  }, []);
 
   useEffect(() => {
     document.title = "山角行 - 當地活動";
     // jquery程式碼寫在這裡
-    $(window).on("load resize", function () {
-      if ($(window).width() <= 1043) {
-        // 快速選單
-        $(".quick-list").addClass("fixed-bottom");
-      } else {
-        $(".quick-list").removeClass("fixed-bottom");
-      }
-    });
-  }, []);
+    // $(window).on("load resize", function () {
+    if ($(window).width() <= 1047) {
+      // 快速選單
+      $(".quick-list").addClass("fixed-bottom");
+    } else {
+      $(".quick-list").removeClass("fixed-bottom");
+    }
+    // });
+    console.log(load);
+  }, [load]);
 
   return (
     <>
